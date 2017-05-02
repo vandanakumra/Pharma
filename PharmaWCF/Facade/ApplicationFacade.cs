3a8a61076bf3645048bf500b182b56b252ef6fbe
vandana <vandanakumra@yahoo.com>
2017-05-03 00:46:25 +0530

    Added Folders and facade files

---
+++ PharmaWCF/Facade/ApplicationFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PharmaWCF.Facade
+{
+    public class ApplicationFacade : IApplicationFacade
+    {
+    }
+}
