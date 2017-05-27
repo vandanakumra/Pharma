19c7e95960516d06bdd2f8a64bfce2378cfbd23f
Nitin <nitin21ster@gmail.com>
2017-05-28 01:13:22 +0530

    chnages

--- PharmaUI/frmAccountLedgerMasterAddUpdate.Designer.cs
+++ PharmaUI/frmAccountLedgerMasterAddUpdate.Designer.cs
@@ -48,6 +48,8 @@
             this.btnCancel = new System.Windows.Forms.Button();
             this.cbxStatus = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtSalePurchaseValue = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.gbBalanceSheet.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -66,9 +68,9 @@
             this.cbAccountLedgerType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbAccountLedgerType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbAccountLedgerType.FormattingEnabled = true;
-            this.cbAccountLedgerType.Location = new System.Drawing.Point(163, 65);
+            this.cbAccountLedgerType.Location = new System.Drawing.Point(167, 65);
             this.cbAccountLedgerType.Name = "cbAccountLedgerType";
-            this.cbAccountLedgerType.Size = new System.Drawing.Size(212, 24);
+            this.cbAccountLedgerType.Size = new System.Drawing.Size(301, 24);
             this.cbAccountLedgerType.TabIndex = 1;
             // 
             // lblAccNo
@@ -77,18 +79,18 @@
             this.lblAccNo.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblAccNo.Location = new System.Drawing.Point(33, 104);
             this.lblAccNo.Name = "lblAccNo";
-            this.lblAccNo.Size = new System.Drawing.Size(113, 19);
+            this.lblAccNo.Size = new System.Drawing.Size(87, 19);
             this.lblAccNo.TabIndex = 2;
-            this.lblAccNo.Text = "Account Number";
+            this.lblAccNo.Text = "Ledger Code";
             // 
             // txtAccountLedgerCode
             // 
             this.txtAccountLedgerCode.Enabled = false;
             this.txtAccountLedgerCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAccountLedgerCode.Location = new System.Drawing.Point(163, 102);
+            this.txtAccountLedgerCode.Location = new System.Drawing.Point(167, 102);
             this.txtAccountLedgerCode.Name = "txtAccountLedgerCode";
             this.txtAccountLedgerCode.ReadOnly = true;
-            this.txtAccountLedgerCode.Size = new System.Drawing.Size(212, 23);
+            this.txtAccountLedgerCode.Size = new System.Drawing.Size(301, 23);
             this.txtAccountLedgerCode.TabIndex = 2;
             // 
             // label3
@@ -97,23 +99,23 @@
             this.label3.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(33, 173);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(99, 19);
+            this.label3.Size = new System.Drawing.Size(91, 19);
             this.label3.TabIndex = 4;
-            this.label3.Text = "Account Name";
+            this.label3.Text = "Ledger Name";
             // 
             // tbAccountName
             // 
             this.tbAccountName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.tbAccountName.Location = new System.Drawing.Point(163, 173);
+            this.tbAccountName.Location = new System.Drawing.Point(167, 173);
             this.tbAccountName.Name = "tbAccountName";
-            this.tbAccountName.Size = new System.Drawing.Size(212, 23);
+            this.tbAccountName.Size = new System.Drawing.Size(301, 23);
             this.tbAccountName.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(33, 243);
+            this.label4.Location = new System.Drawing.Point(33, 271);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(112, 19);
             this.label4.TabIndex = 6;
@@ -122,10 +124,10 @@
             // tbOpeningBalance
             // 
             this.tbOpeningBalance.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.tbOpeningBalance.Location = new System.Drawing.Point(163, 244);
+            this.tbOpeningBalance.Location = new System.Drawing.Point(167, 272);
             this.tbOpeningBalance.Name = "tbOpeningBalance";
-            this.tbOpeningBalance.Size = new System.Drawing.Size(212, 23);
-            this.tbOpeningBalance.TabIndex = 6;
+            this.tbOpeningBalance.Size = new System.Drawing.Size(243, 23);
+            this.tbOpeningBalance.TabIndex = 7;
             this.tbOpeningBalance.Text = "0.00";
             this.tbOpeningBalance.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbOpeningBalance_KeyPress);
             // 
@@ -137,19 +139,19 @@
             this.cbDebitCredit.Items.AddRange(new object[] {
             "C",
             "D"});
-            this.cbDebitCredit.Location = new System.Drawing.Point(381, 243);
+            this.cbDebitCredit.Location = new System.Drawing.Point(416, 271);
             this.cbDebitCredit.Name = "cbDebitCredit";
             this.cbDebitCredit.Size = new System.Drawing.Size(52, 24);
-            this.cbDebitCredit.TabIndex = 7;
+            this.cbDebitCredit.TabIndex = 8;
             // 
             // cbAccountType
             // 
             this.cbAccountType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbAccountType.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbAccountType.FormattingEnabled = true;
-            this.cbAccountType.Location = new System.Drawing.Point(163, 134);
+            this.cbAccountType.Location = new System.Drawing.Point(167, 134);
             this.cbAccountType.Name = "cbAccountType";
-            this.cbAccountType.Size = new System.Drawing.Size(212, 24);
+            this.cbAccountType.Size = new System.Drawing.Size(301, 24);
             this.cbAccountType.TabIndex = 3;
             // 
             // label5
@@ -158,9 +160,9 @@
             this.label5.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.Location = new System.Drawing.Point(33, 136);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(92, 19);
+            this.label5.Size = new System.Drawing.Size(84, 19);
             this.label5.TabIndex = 9;
-            this.label5.Text = "Account Type";
+            this.label5.Text = "Ledger Type";
             // 
             // cbDebitControlCode
             // 
@@ -169,8 +171,8 @@
             this.cbDebitControlCode.FormattingEnabled = true;
             this.cbDebitControlCode.Location = new System.Drawing.Point(130, 29);
             this.cbDebitControlCode.Name = "cbDebitControlCode";
-            this.cbDebitControlCode.Size = new System.Drawing.Size(212, 24);
-            this.cbDebitControlCode.TabIndex = 8;
+            this.cbDebitControlCode.Size = new System.Drawing.Size(302, 24);
+            this.cbDebitControlCode.TabIndex = 9;
             // 
             // label6
             // 
@@ -189,8 +191,8 @@
             this.cbCreditControlCode.FormattingEnabled = true;
             this.cbCreditControlCode.Location = new System.Drawing.Point(130, 68);
             this.cbCreditControlCode.Name = "cbCreditControlCode";
-            this.cbCreditControlCode.Size = new System.Drawing.Size(212, 24);
-            this.cbCreditControlCode.TabIndex = 9;
+            this.cbCreditControlCode.Size = new System.Drawing.Size(302, 24);
+            this.cbCreditControlCode.TabIndex = 10;
             // 
             // label7
             // 
@@ -209,9 +211,9 @@
             this.gbBalanceSheet.Controls.Add(this.label6);
             this.gbBalanceSheet.Controls.Add(this.label7);
             this.gbBalanceSheet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbBalanceSheet.Location = new System.Drawing.Point(26, 273);
+            this.gbBalanceSheet.Location = new System.Drawing.Point(26, 301);
             this.gbBalanceSheet.Name = "gbBalanceSheet";
-            this.gbBalanceSheet.Size = new System.Drawing.Size(438, 116);
+            this.gbBalanceSheet.Size = new System.Drawing.Size(442, 116);
             this.gbBalanceSheet.TabIndex = 15;
             this.gbBalanceSheet.TabStop = false;
             this.gbBalanceSheet.Text = "Balance Sheet Codes";
@@ -219,7 +221,7 @@
             // btnSave
             // 
             this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSave.Location = new System.Drawing.Point(129, 409);
+            this.btnSave.Location = new System.Drawing.Point(172, 440);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(83, 48);
             this.btnSave.TabIndex = 21;
@@ -230,7 +232,7 @@
             // btnCancel
             // 
             this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCancel.Location = new System.Drawing.Point(237, 409);
+            this.btnCancel.Location = new System.Drawing.Point(280, 440);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(83, 48);
             this.btnCancel.TabIndex = 22;
@@ -242,27 +244,48 @@
             // 
             this.cbxStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbxStatus.FormattingEnabled = true;
-            this.cbxStatus.Location = new System.Drawing.Point(163, 211);
+            this.cbxStatus.Location = new System.Drawing.Point(167, 239);
             this.cbxStatus.Name = "cbxStatus";
-            this.cbxStatus.Size = new System.Drawing.Size(212, 21);
-            this.cbxStatus.TabIndex = 5;
+            this.cbxStatus.Size = new System.Drawing.Size(301, 21);
+            this.cbxStatus.TabIndex = 6;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(33, 211);
+            this.label2.Location = new System.Drawing.Point(33, 239);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 19);
             this.label2.TabIndex = 26;
             this.label2.Text = "Status";
             // 
+            // txtSalePurchaseValue
+            // 
+            this.txtSalePurchaseValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtSalePurchaseValue.Location = new System.Drawing.Point(167, 205);
+            this.txtSalePurchaseValue.Name = "txtSalePurchaseValue";
+            this.txtSalePurchaseValue.Size = new System.Drawing.Size(301, 23);
+            this.txtSalePurchaseValue.TabIndex = 5;
+            this.txtSalePurchaseValue.Text = "0.00";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(33, 205);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 19);
+            this.label8.TabIndex = 28;
+            this.label8.Text = "Sale/Purchase Value";
+            // 
             // frmAccountLedgerMasterAddUpdate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 485);
+            this.ClientSize = new System.Drawing.Size(564, 515);
             this.ControlBox = false;
+            this.Controls.Add(this.txtSalePurchaseValue);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.cbxStatus);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnCancel);
@@ -313,5 +336,7 @@
         private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.ComboBox cbxStatus;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSalePurchaseValue;
+        private System.Windows.Forms.Label label8;
     }
 }
