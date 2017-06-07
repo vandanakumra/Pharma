0ea45880dcdce962127b3572eca43b4718b8300b
Nitin <nitin21ster@gmail.com>
2017-06-08 01:09:55 +0530

    done

--- PharmaBusinessObjects/IApplicationFacade.cs
+++ PharmaBusinessObjects/IApplicationFacade.cs
@@ -139,7 +139,7 @@ namespace PharmaBusinessObjects
         long InsertUpdateTempReceiptPayment(PharmaBusinessObjects.Transaction.ReceiptPayment.ReceiptPaymentItem receiptPayment);
         void InsertTempBillAdjustment(List<PharmaBusinessObjects.Transaction.ReceiptPayment.BillAdjusted> billAdjustmentList);
         void ClearTempBillAdjustment(TransactionEntity transactionEntity);
-
+        bool DeleteUnSavedData(long purchaseSaleBookHeaderID);
         List<SaleChangeType> GetSaleEntryChangeTypes();
         #endregion
     }
